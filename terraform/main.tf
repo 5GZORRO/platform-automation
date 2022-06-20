6d3c3387b519f702959864a5afd05bc5f4e47f0c
ffais <ffais@fbk.eu>
2022-06-20 16:35:07 +0200

    add aks node public key

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,25 +4,25 @@ provider "azurerm" {
 }
 
 module "aks" {
-  source = "./modules/aks" 
+  source = "./modules/aks"
 
-  subscription_id = var.subscription_id
-  tags = var.tags
-  node_count = var.node_count
-  ssh_public_key = var.ssh_public_key
-  dns_prefix = var.dns_prefix
-  cluster_name = var.cluster_name
-  resource_group_name = var.resource_group_name
-  location = var.location
+  subscription_id        = var.subscription_id
+  tags                   = var.tags
+  node_count             = var.node_count
+  ssh_public_key         = var.ssh_public_key
+  dns_prefix             = var.dns_prefix
+  cluster_name           = var.cluster_name
+  resource_group_name    = var.resource_group_name
+  location               = var.location
   default_node_pool_name = var.default_node_pool_name
   default_node_pool_size = var.default_node_pool_size
-  kubernetes_version = var.kubernetes_version
-  max_pods = var.max_pods
+  kubernetes_version     = var.kubernetes_version
+  max_pods               = var.max_pods
 }
 
 provider "helm" {
   kubernetes {
-    host     = module.aks.host
+    host = module.aks.host
 
     client_certificate     = base64decode(module.aks.client_certificate)
     client_key             = base64decode(module.aks.client_key)
@@ -31,7 +31,7 @@ provider "helm" {
 }
 
 module "helm" {
-  source = "./modules/helm"  
+  source = "./modules/helm"
 
   values-file-path = var.values-file-path
 }
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,19 +1,19 @@
 subscription_id = "f8948095-f2ff-4dbd-aa56-d319a3c81cf1"
 tags = {
-  "project" = "5gzorro"
-  "unit"    = "DSL"
+  "project"     = "5gzorro"
+  "unit"        = "DSL"
   "cost-centre" = "DS001"
-  "job-order" = "IT001-0101362"
+  "job-order"   = "IT001-0101362"
 }
-node_count = 2
-ssh_public_key = "~/Documenti/public/5gzorro.pub"
-dns_prefix = "project5gzorro"
-cluster_name = "5gzorro"
-resource_group_name = "5gzorro"
-location = "germanywestcentral"
+node_count             = 2
+ssh_public_key         = "~/Documenti/public/5gzorro.pub"
+dns_prefix             = "project5gzorro"
+cluster_name           = "5gzorro"
+resource_group_name    = "5gzorro"
+location               = "germanywestcentral"
 default_node_pool_name = "system"
 default_node_pool_size = "Standard_D4s_v5"
-kubernetes_version = "1.23.5"
-max_pods = 250
+kubernetes_version     = "1.23.5"
+max_pods               = 250
 
 values-file-path = "5gzorro-platform-values.yaml"
