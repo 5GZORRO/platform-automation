34feea064daa565351db8104b51c3372498634fa
ffais <ffais@fbk.eu>
2022-08-23 19:19:18 +0200

    fix azure dns

--- terraform/modules/commons/main.tf
+++ terraform/modules/commons/main.tf
@@ -20,8 +20,8 @@ resource "azurerm_dns_zone" "default" {
   tags                = var.tags
 
   soa_record {
-    email = "sysadmin@smartcommunitylab.it"
-    hostname = "ns1-02.azure-dns.com."
+    email = "azuredns-hostmaster.microsoft.com"
+    host_name = "ns1-02.azure-dns.com."
     refresh_time = 360
     ttl = 360
   }
