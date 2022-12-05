02ec02a88c11bdc5dc93a135de371418682eecc7
ffais <ffais@fbk.eu>
2022-12-05 10:08:30 +0100

    fix sensitive

--- terraform/output.tf
+++ terraform/output.tf
@@ -32,13 +32,16 @@ output "dns_name_servers" {
 }
 
 output "client_key" {
-  value = module.aks.client_key
+  value     = module.aks.client_key
+  sensitive = true
 }
 
 output "client_certificate" {
-  value = module.aks.client_certificate
+  value     = module.aks.client_certificate
+  sensitive = true
 }
 
 output "cluster_ca_certificate" {
-  value = module.aks.cluster_ca_certificate
+  value     = module.aks.cluster_ca_certificate
+  sensitive = true
 }
